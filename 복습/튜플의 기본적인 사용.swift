2c11cabcd083a4caec207fe4c7fe8711a5133790
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2022-10-29 16:54:45 +0900

    Update 튜플의 기본적인 사용.swift

--- 복습/튜플의 기본적인 사용.swift
+++ 복습/튜플의 기본적인 사용.swift
@@ -16,3 +16,34 @@ for index in tupleArr {
     print(index.1) // "Hello, world!" "Hello, world!"
     print(index.2) // true false
 }
+
+// <Naming Index>
+
+var namedTuple = (name: "Bill", age: 30, likes : ["Swift", "iOS"])
+print(namedTuple.name) // Bill
+print(namedTuple.age) // 30
+print(namedTuple.likes) // [“Swift”,"iOS"]
+
+namedTuple.name = "Joyce" // name을 다른 값으로 변경 가능
+print(namedTuple.name) // Joyce
+
+// <튜플 타입>
+
+// 입력하는 데이터 타입에는 제한이 없으며 튜플 타입도 저장이 가능하다.
+var anotherTuple = (1, (tuple)) 
+print(anotherTuple.0) // 1
+print(anotherTuple.1.0) // Bill
+
+// <Function Type>
+
+// 튜플은 아래와 같이 함수 타입(Function Type) 또한 저장 가능합니다.
+
+func a() -> Int { return 1 }
+func b() -> String { return "Bill" }
+func c() -> Bool { return false }
+
+var functionTuple = (a(), b(), c())
+
+print(functionTuple.0) // 1
+print(functionTuple.1) // Bill
+print(functionTuple.2) // false
